2038abb0b9ea3aa20a59402caecc48b76505121f
oscar <subirats.oscar@gmail.com>
2016-04-11 12:05:28 +0200

    dty

---
+++ 11deabril/src/test/java/com/indra/formacio/SaleTest.java
@@ -0,0 +1,18 @@
+package com.indra.formacio;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
+import junit.framework.TestCase;
+
+import com.indra.formacio.model.Customer;
+import com.indra.formacio.model.Employee;
+
+
+public class SaleTest extends TestCase{
+	
+
+}
